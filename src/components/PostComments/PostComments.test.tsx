270b27ca490f872195423a4b6c0ae0934929b97c
RoFerrigati <ferrigatitrabalho@hotmail.com>
2025-01-23 15:26:47 -0300

    projeto testing library

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+<<<<<<< HEAD
 import Post from '.';
 import PostComment from '.';
 
@@ -8,3 +9,61 @@ describe('Teste para o componente PostComment', () => {
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 });
+=======
+import Post from '.'; 
+
+
+const mock = [
+    {
+        id: 1,
+        comment: 'Olá marilene'
+    },
+    {
+        id: 2,
+        comment: 'Quero um desse'
+    }
+]
+
+
+
+let index = 0
+let mensagem =  mock[index].comment
+
+function mudamensagem(){
+    index = (index + 1) 
+    mensagem = mock[index].comment
+
+}
+
+
+
+describe('Teste para o componente PostComment', () => {
+    
+    it('Deve renderizar o componente corretamente', () => {
+        render(<Post />);
+        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('deve renderizar o primeiro comentario', async () => {
+        render(<Post />)
+        const botao = screen.getByTestId('btn-comentario')
+        const Textarea = screen.getByTestId('textArea')
+        fireEvent.change(Textarea, {target: {value: mensagem}})
+        fireEvent.click(botao)
+        expect(screen.getByText('Olá marilene')).toBeInTheDocument()
+        
+        mudamensagem()
+    })
+
+
+    test('deve renderizar segundo comentario', async () => {
+        render(<Post />)
+        const botao = screen.getByTestId('btn-comentario')
+        const Textarea = screen.getByTestId('textArea')
+        fireEvent.change(Textarea, {target: {value: mensagem}})
+        fireEvent.click(botao)
+        expect(screen.getByText('Quero um desse')).toBeInTheDocument()
+        
+    })
+});
+>>>>>>> 8bd4c13 (testing library)
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -6,7 +6,11 @@ import Comment from '../../models/Comment';
 const Post = () => {
     const [comments, setComments] = useState<Comment[]>([]);
     const [tempComment, setTempComment] = useState('');
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8bd4c13 (testing library)
     function handleAddComment(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const newComment = new Comment(comments.length, tempComment);
@@ -16,18 +20,30 @@ const Post = () => {
 
     return (
         <div>
+<<<<<<< HEAD
             <ul className={styles['post-comments']}>
                 {comments.map(({ comment, id }) => (
                     <li className={styles['post-comment']} key={id}>
                         <p className={styles['post-comment-content']}>
+=======
+            <ul  className={styles['post-comments']}>
+                {comments.map(({ comment, id }) => (
+                    <li className={styles['post-comment']} key={id}>
+                        <p  className={styles['post-comment-content']}>
+>>>>>>> 8bd4c13 (testing library)
                             {comment}
                         </p>
                     </li>
                 ))}
             </ul>
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
+<<<<<<< HEAD
                 <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
                 <button type="submit" className={styles['post-comments-form-button']}>
+=======
+                <textarea data-testid="textArea" value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
+                <button data-testid="btn-comentario" type="submit" className={styles['post-comments-form-button']}>
+>>>>>>> 8bd4c13 (testing library)
                     Comentar
                 </button>
             </form>
